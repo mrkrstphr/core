389080ce98006fac548aa10ee7799f3f8f2b77f7
Kristopher Wilson <kristopherwilson@gmail.com>
2013-08-19 17:41:18 -0400

    fleshing out TriggerAbstract

--- src/Martha/Core/Job/Trigger/ScmChange.php
+++ src/Martha/Core/Job/Trigger/ScmChange.php
@@ -2,32 +2,10 @@
 
 namespace Martha\Core\Job\Trigger;
 
-use Martha\Scm\ChangeSet\ChangeSet;
-
 /**
  * Class ScmChange
  * @package Martha\Core\Job\Trigger
  */
 class ScmChange extends TriggerAbstract
 {
-    /**
-     * @var \Martha\Scm\ChangeSet\ChangeSet
-     */
-    protected $changeSet;
-
-    /**
-     * @param \Martha\Scm\ChangeSet\ChangeSet $changeSet
-     */
-    public function setChangeSet($changeSet)
-    {
-        $this->changeSet = $changeSet;
-    }
-
-    /**
-     * @return \Martha\Scm\ChangeSet\ChangeSet $changeSet
-     */
-    public function getChangeSet()
-    {
-        return $this->changeSet;
-    }
 }
--- src/Martha/Core/Job/Trigger/TriggerAbstract.php
+++ src/Martha/Core/Job/Trigger/TriggerAbstract.php
@@ -2,10 +2,58 @@
 
 namespace Martha\Core\Job\Trigger;
 
+use Martha\Scm\ChangeSet\ChangeSet;
+use Martha\Scm\Repository;
+
 /**
  * Class TriggerAbstract
  * @package Martha\Core\Job\Trigger
  */
 abstract class TriggerAbstract
 {
+    /**
+     * @var Repository
+     */
+    protected $repository;
+
+    /**
+     * @var ChangeSet
+     */
+    protected $changeSet;
+
+    /**
+     * @param \Martha\Scm\Repository $repository
+     * @return $this
+     */
+    public function setRepository($repository)
+    {
+        $this->repository = $repository;
+        return $this;
+    }
+
+    /**
+     * @return \Martha\Scm\Repository
+     */
+    public function getRepository()
+    {
+        return $this->repository;
+    }
+
+    /**
+     * @param \Martha\Scm\ChangeSet\ChangeSet $changeSet
+     * @return $this
+     */
+    public function setChangeSet($changeSet)
+    {
+        $this->changeSet = $changeSet;
+        return $this;
+    }
+
+    /**
+     * @return \Martha\Scm\ChangeSet\ChangeSet
+     */
+    public function getChangeSet()
+    {
+        return $this->changeSet;
+    }
 }
